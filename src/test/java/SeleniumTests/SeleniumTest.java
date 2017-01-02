04675cc9878700fd011a76256116910da30c5030
Wiktor Żołnowski <wiktor.zolnowski@pragmaticcoders.com>
2017-01-02 13:45:03 +0100

    Add Selenium Base clase that every test will be inheriting from

---
+++ src/test/java/SeleniumTests/SeleniumTest.java
@@ -0,0 +1,28 @@
+package SeleniumTests;
+
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by streser on 02.01.2017.
+ */
+public class SeleniumTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        //driver = new FirefoxDriver();
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().deleteAllCookies();
+    }
+}
