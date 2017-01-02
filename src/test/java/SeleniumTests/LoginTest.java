7008425d89425893d3dae9beb8d71b3e2c5c4d74
Wiktor Żołnowski <wiktor.zolnowski@pragmaticcoders.com>
2017-01-02 13:38:42 +0100

    Add JUnit dependency

--- src/test/java/SeleniumTests/LoginTest.java
+++ src/test/java/SeleniumTests/LoginTest.java
@@ -1,9 +1,12 @@
 package SeleniumTests;
 
+import org.junit.Test;
+
 /**
  * Created by streser on 02.01.2017.
  */
 public class LoginTest extends SeleniumTest {
+    @Test
     public void shouldBePossibleToLogIn() {
         LoginPage lp = new LoginPage(driver);
         lp.open();
